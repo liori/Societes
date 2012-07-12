80df5271774eae9b51482320a24326e69e7a9ec8
Daniel Dumaresq <danquebec@singularity.fr>
2012-07-11 20:03:09 -0400

    will you fucking commit?

--- societes.py
+++ societes.py
@@ -7,16 +7,18 @@ from map1 import *
 
 #---COLORS-------------------------------------------------------------
 
-#           R   G    B
+#              R   G    B
 
-WHITE =  ( 255, 255, 255)
-BLACK =  (   0,   0,   0)
-BLUE =   (   0,   0, 255)
-GREEN =  (   0, 255,   0)
-RED =    ( 255,   0,   0)
-YELLOW = ( 255, 250,  60)
-ORANGE = ( 255, 150,  30)
-BROWN =  ( 150,  50,   0)
+WHITE =     ( 255, 255, 255)
+BLACK =     (   0,   0,   0)
+BLUE =      (   0,   0, 255)
+GREEN =     (   0, 255,   0)
+RED =       ( 255,   0,   0)
+YELLOW =    ( 255, 250,  60)
+ORANGE =    ( 255, 150,  30)
+BROWN =     ( 150,  50,   0)
+
+INVISIBLE = (   0,   0,   0,   0)
 
 
 #----------------------------------------------------------------------
@@ -25,7 +27,11 @@ BROWN =  ( 150,  50,   0)
 
 FPS = 30
 
+FONT = 'freesansbold.ttf'
+FONTSIZE = 16
+
 # These are the blocks sizes. They are in the configuration file “societes.conf”.
+# TOLEARN: ConfigParser
 conf = open('societes.conf', 'r')
 
 conf.seek(98, 0)
@@ -43,13 +49,16 @@ SCREENSIZEy = 600
 INTERFACESIZEx = SCREENSIZEx
 INTERFACESIZEy = 100
 
-OUTTERNEXTBUTTONSIZE = 60
-OUTTERNEXTBUTTONPOSx = 920
-OUTTERNEXTBUTTONPOSy = SCREENSIZEy - (OUTTERNEXTBUTTONSIZE + 20)
+OUTTERFINISHBUTTONSIZE = 60
+OUTTERFINISHBUTTONPOSx = 920
+OUTTERFINISHBUTTONPOSy = SCREENSIZEy - (OUTTERFINISHBUTTONSIZE + 20)
 
-INNERNEXTBUTTONSIZE = OUTTERNEXTBUTTONSIZE / 2
-INNERNEXTBUTTONPOSx = OUTTERNEXTBUTTONPOSx + (OUTTERNEXTBUTTONSIZE / 4)
-INNERNEXTBUTTONPOSy = OUTTERNEXTBUTTONPOSy + (OUTTERNEXTBUTTONSIZE / 4)
+INNERFINISHBUTTONSIZE = OUTTERFINISHBUTTONSIZE / 2
+INNERFINISHBUTTONPOSx = OUTTERFINISHBUTTONPOSx + (OUTTERFINISHBUTTONSIZE / 4)
+INNERFINISHBUTTONPOSy = OUTTERFINISHBUTTONPOSy + (OUTTERFINISHBUTTONSIZE / 4)
+
+POPTEXTRIGHT = SCREENSIZEx - 50
+POPTEXTTOP = 30
 
 
 
@@ -59,8 +68,8 @@ BGCOLOR = BLACK
 
 INTERFACEZONECOLOR = ORANGE
 
-OUTTERNEXTBUTTONCOLOR = BROWN
-INNERNEXTBUTTONCOLOR = RED
+OUTTERFINISHBUTTONCOLOR = BROWN
+INNERFINISHBUTTONCOLOR = RED
 
 
 #---PLACES----------------------------------------------------------
@@ -74,18 +83,22 @@ THE_OTHER = YELLOW
 
 pygame.init()
 
-
 #------------------------------------------------------------------
 
 def main():
-    global FPSCLOCK, DISPLAYSURF
+    global FPSCLOCK, DISPLAYSURF, ALPHADISPLAYSURF
     FPSCLOCK = pygame.time.Clock()
     
     mousex = 0
     mousey = 0
     
+    fontObj = pygame.font.Font(FONT, FONTSIZE)
+    
     # What’s the map?
     theMap = createMap()
+    
+    # Calculate stuff about the faction
+    pop = calcFaction(theMap)
     
     # How big is it?
     NUMBERBLOCKSx, NUMBERBLOCKSy = countMap(theMap)
@@ -94,19 +107,18 @@ def main():
     for other_column in theMap[1:]:
         assert len(other_column) == len(theMap[0]), 'The columns aren’t equal.'
     
-
-    
     # Make the screen.
     DISPLAYSURF = pygame.display.set_mode((SCREENSIZEx, SCREENSIZEy))
+    ALPHADISPLAYSURF = DISPLAYSURF.convert_alpha()
     Wtf = ((BLOCKSIZEx * (NUMBERBLOCKSx)), (BLOCKSIZEy * (NUMBERBLOCKSy + INTERFACESIZEy)))
     pygame.display.set_caption('Sociétés')
     
     
-    
     DISPLAYSURF.fill(BGCOLOR)
     
-    while True: # main game loop
-        mainLoop(theMap, NUMBERBLOCKSx, NUMBERBLOCKSy, mousex, mousey)
+    mainLoop(theMap, NUMBERBLOCKSx, NUMBERBLOCKSy, mousex, mousey, pop, fontObj)
+#-------------------------------------------------------------------------
+
 
 def leftTopCoordsOfBlock(smthx, smthy):
     # Convert map coordinates to pixel coordinates.
@@ -114,7 +126,7 @@ def leftTopCoordsOfBlock(smthx, smthy):
     top = smthy * BLOCKSIZEy
     return (left, top)
 
-def getSomethingAtPixel(x, y, NUMBERBLOCKSx, NUMBERBLOCKSy, OUTTERNEXTBUTTONPOSx, OUTTERNEXTBUTTONPOSy, OUTTERNEXTBUTTONSIZE):
+def getSomethingAtPixel(x, y, NUMBERBLOCKSx, NUMBERBLOCKSy, OUTTERFINISHBUTTONPOSx, OUTTERFINISHBUTTONPOSy, OUTTERFINISHBUTTONSIZE):
     
     for smthx in range(NUMBERBLOCKSx):
         for smthy in range(NUMBERBLOCKSy):
@@ -124,11 +136,14 @@ def getSomethingAtPixel(x, y, NUMBERBLOCKSx, NUMBERBLOCKSy, OUTTERNEXTBUTTONPOSx
             if blockRect.collidepoint(x, y):
                 return (smth)
     
-    nextButtonRect = pygame.Rect(OUTTERNEXTBUTTONPOSx, OUTTERNEXTBUTTONPOSy, OUTTERNEXTBUTTONSIZE, OUTTERNEXTBUTTONSIZE)
+    nextButtonRect = pygame.Rect(OUTTERFINISHBUTTONPOSx, OUTTERFINISHBUTTONPOSy, OUTTERFINISHBUTTONSIZE, OUTTERFINISHBUTTONSIZE)
     if nextButtonRect.collidepoint(x, y):
-        return ('nextButtonClick')
+        return ('finishButtonClick')
     
     return (None)
+
+
+#---STRICTLY GAME RELATED STUFF------------------------------------
 
 def getColor(theMap, smthx, smthy):
     # color value for x, y spot is stored in TheMap[x][y]['faction'].
@@ -144,25 +159,47 @@ def drawMap(theMap, NUMBERBLOCKSx, NUMBERBLOCKSy):
             color = getColor(theMap, smthx, smthy)
             pygame.draw.rect(DISPLAYSURF, color, (left, top, BLOCKSIZEx, BLOCKSIZEy))
 
+def calcFaction(theMap):
+    yourRegions = []
+    for col in theMap:
+        for region in col:
+            if region['faction'] == YOU:
+                yourRegions.append(region)
+    
+    pop = 0
+    for region in yourRegions:
+        pop = pop + region['pop']
+    
+    return pop
+
 #----INTERFACE DRAWING--------------
 
-def drawInterface():
+def drawInterface(fontObj, pop):
     # Draw the interface.
     # It’s used in main.
     interfaceZone()
-    nextButton()
+    finishButton()
+    someText(fontObj, pop)
 
 def interfaceZone():
     InterfaceRect = pygame.draw.rect(DISPLAYSURF, INTERFACEZONECOLOR, (0, (SCREENSIZEy - INTERFACESIZEy), INTERFACESIZEx, INTERFACESIZEy))
 
-def nextButton():
-    OutterNextButton = pygame.draw.rect(DISPLAYSURF, OUTTERNEXTBUTTONCOLOR, (OUTTERNEXTBUTTONPOSx, OUTTERNEXTBUTTONPOSy, OUTTERNEXTBUTTONSIZE, OUTTERNEXTBUTTONSIZE))
-    InnerNextButton = pygame.draw.rect(DISPLAYSURF, INNERNEXTBUTTONCOLOR, (INNERNEXTBUTTONPOSx, INNERNEXTBUTTONPOSy, INNERNEXTBUTTONSIZE, INNERNEXTBUTTONSIZE))
+def finishButton():
+    OutterFinishButton = pygame.draw.rect(DISPLAYSURF, OUTTERFINISHBUTTONCOLOR, (OUTTERFINISHBUTTONPOSx, OUTTERFINISHBUTTONPOSy, OUTTERFINISHBUTTONSIZE, OUTTERFINISHBUTTONSIZE))
+    InnerFinishButton = pygame.draw.rect(DISPLAYSURF, INNERFINISHBUTTONCOLOR, (INNERFINISHBUTTONPOSx, INNERFINISHBUTTONPOSy, INNERFINISHBUTTONSIZE, INNERFINISHBUTTONSIZE))
 
+def someText(fontObj, pop):
+    popTextSurface = fontObj.render('Population: {}'.format(pop), True, WHITE, INVISIBLE)
+    popTextRect = popTextSurface.get_rect()
+    popTextRect.right = POPTEXTRIGHT
+    popTextRect.top = POPTEXTTOP
+    DISPLAYSURF.blit(popTextSurface, popTextRect)
 
-
-
-
+def finishTurn(pop):
+    # Oulah…
+    pop = pop * 1.10
+    return pop
+    
 #-----------------------------------------
 
 def countMap(theMap):
@@ -171,7 +208,10 @@ def countMap(theMap):
     nbRows = len(theMap[0])
     return nbCol, nbRows
 
-def mainLoop(theMap, NUMBERBLOCKSx, NUMBERBLOCKSy, mousex, mousey):
+#---MAIN LOOP-----------------------------------------------------
+
+def mainLoop(theMap, NUMBERBLOCKSx, NUMBERBLOCKSy, mousex, mousey, pop, fontObj):
+    while True: # main game loop
         mouseClicked = False 
         
         # Draw the map.
@@ -179,8 +219,10 @@ def mainLoop(theMap, NUMBERBLOCKSx, NUMBERBLOCKSy, mousex, mousey):
         # The map has to be smaller than the screen + the interface.
         assert NUMBERBLOCKSx * BLOCKSIZEx < 1001 and NUMBERBLOCKSy * BLOCKSIZEy < 601, 'La carte est trop grande. La hauteur doit être moins grande que 1001 pixels et la largeur doit être moins grande que 601 pixels.'
         
+        
         # Draw the interface.
-        drawInterface()
+        drawInterface(fontObj, pop)
+        
         
         # What happens?
         for event in pygame.event.get():
@@ -194,15 +236,16 @@ def mainLoop(theMap, NUMBERBLOCKSx, NUMBERBLOCKSy, mousex, mousey):
                 mouseClicked = True
         
         # Where did you click? Was it on a block? What block?
-        smth = getSomethingAtPixel(mousex, mousey, NUMBERBLOCKSx, NUMBERBLOCKSy, OUTTERNEXTBUTTONPOSx, OUTTERNEXTBUTTONPOSy, OUTTERNEXTBUTTONSIZE)
+        smth = getSomethingAtPixel(mousex, mousey, NUMBERBLOCKSx, NUMBERBLOCKSy, OUTTERFINISHBUTTONPOSx, OUTTERFINISHBUTTONPOSy, OUTTERFINISHBUTTONSIZE)
         if type(smth) == tuple: # If it was on the map.
             smthx = smth[0]
             smthy = smth[1]
             if mouseClicked:
                 print '{}, {}'.format(smthx, smthy)
         
-        if smth == 'nextButtonClick' and mouseClicked:
-            print 'nextButtonClick'
+        if smth == 'finishButtonClick' and mouseClicked:
+            pop = finishTurn(pop)
+            print pop
         
         
         pygame.display.update()
